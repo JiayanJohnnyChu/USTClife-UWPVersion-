53c972ccda3d635e7da7b9cc54f19e50f8578dcd
家言 禇 <cjy_johnny@outlook.com>
2021-08-18 18:24:15 +0800

    添加项目文件。

---
+++ USTClife(uwp)/App1/MainPage.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x804 上介绍了“空白页”项模板
+
+namespace App1
+{
+    /// <summary>
+    /// 可用于自身或导航至 Frame 内部的空白页。
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void NvAll_ItemInvoked(NavigationView sender, NavigationViewItemInvokedEventArgs args)
+        {
+            //先判断是否选中了setting
+            if (args.IsSettingsInvoked)
+            {
+                ContentFrame.Navigate(typeof(SettingsPage));
+            }
+            else
+            {
+                //选中项的内容
+                switch (args.InvokedItemContainer.Tag)
+                {
+                    case "MostClicked":
+                        ContentFrame.Navigate(typeof(MostClicked));
+                        break;
+                    case "LearningInUSTC":
+                        ContentFrame.Navigate(typeof(LearningInUSTC));
+                        break;
+                }
+            }
+        }
+    }
+}
